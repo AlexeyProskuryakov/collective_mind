0f7d0fea03dc70c0dc6d86042e97cf7aa8321972
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2014-10-22 23:35:19 +0400

    searcher 2

--- searchengine/engine.py
+++ searchengine/engine.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from itertools import chain
+from collections import defaultdict, Iterable
 from urlparse import urljoin, urlparse
 from redis.client import StrictRedis
 import requests
@@ -10,11 +10,14 @@ __author__ = '4ikist'
 
 
 def is_resident_link(page_url, link):
-    parsed_uri_page = urlparse(page_url)
-    domain_page = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri_page)
-    parsed_uri_page_link = urlparse(link)
-    domain_link = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri_page_link)
-    return domain_page == domain_link
+    try:
+        parsed_uri_page = urlparse(page_url)
+        domain_page = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri_page)
+        parsed_uri_page_link = urlparse(link)
+        domain_link = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri_page_link)
+        return domain_page == domain_link
+    except UnicodeEncodeError as e:
+        return False
 
 
 id = lambda x: hash(x)
@@ -29,8 +32,16 @@ ignore_words = []
 
 
 class DataBaseHandler(object):
-    def __init__(self):
+    def __init__(self, truncate=False):
         self.redis = StrictRedis(db=15)
+        if truncate:
+            self.redis.flushdb()
+
+    def get_url(self, url_id):
+        return self.redis.hget('url_ids', url_id)
+
+    def get_word(self, word_id):
+        return self.redis.hget('word_ids', word_id)
 
     def add_url(self, url):
         url_id = id(url)
@@ -66,13 +77,16 @@ class DataBaseHandler(object):
         return self.redis.get(id(url))
 
     def get_urls_locations_of_(self, word):
+        """
+        :returns {url_id:location,...}
+        """
         word_id = id(word)
         return self.redis.hgetall(url_location(word_id))
 
 
 class Crawler(object):
-    def __init__(self, db_name=None):
-        self.db = DataBaseHandler()
+    def __init__(self, db):
+        self.db = db
 
     def get_entry_id(self, table, field, value, create_new=True):
         return None
@@ -89,6 +103,7 @@ class Crawler(object):
                 self.db.set_word_location(url_id, words_id, i)
 
     def get_text_only(self, soup):
+        #todo use some more good renderer like html 2 text
         v = soup.string
         if v is None:
             elements = []
@@ -103,6 +118,7 @@ class Crawler(object):
         return v.strip()
 
     def separate_words(self, text):
+        print text
         return tokenizers.simple_word_tokenize(text)
 
     def is_indexed(self, url):
@@ -135,30 +151,48 @@ class Crawler(object):
             pages = new_pages
 
 
+def _union_dict_values(one, two):
+    """
+    Объединение двух словарей по значениям
+    :param one:
+    :param two:
+    :return:
+    """
+
+    def add(acc, object):
+        if isinstance(object, Iterable):
+            acc.extend(list(object))
+        elif object is not None:
+            acc.append(object)
+
+    result = defaultdict(tuple)
+    for key in set(one.keys() + two.keys()):
+        value = []
+        add(value, one.get(key))
+        add(value, two.get(key))
+        result[key] = value
+    return result
+
+
 class Searcher(object):
-    def __init__(self):
-        self.db = DataBaseHandler()
-    @staticmethod
-    def __union_dict_values(one, two):
-        """
-        Объединение двух словарей по значениям
-        :param one:
-        :param two:
-        :return:
-        """
-        result = {}
-        for el in chain(one.iterkeys(), two.iterkeys()):
-            result[el] = ()
+    def __init__(self, db):
+        self.db = db
+
 
     def match_rows(self, q):
         words = q.split()
-        result = {}
+        result_ids = {}
         for word in words:
             locations = self.db.get_urls_locations_of_(word)
-
+            result_ids = _union_dict_values(result_ids, locations)
+        result = {}
+        for k, v in result_ids.iteritems():
+            result[self.db.get_url(k)] = v
         return result
 
 
 if __name__ == '__main__':
-    crawler = Crawler('')
-    crawler.crawl(['http://lurkmore.to/'])
+    crawler = Crawler(DataBaseHandler(truncate=True))
+    crawler.crawl(['http://lurkmore.to/'], depth=2, only_resident=True)
+    # searcher = Searcher(DataBaseHandler())
+    # print searcher.match_rows('привет пока')
